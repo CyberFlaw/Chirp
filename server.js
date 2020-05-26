8c9199f370d3a37f6b09e76b5df434b60c044658
CyberFlaw <kannan.pappus@gmail.com>
2020-05-26 19:27:04 +0530

    styles issue fixed

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const experss = require("express");
-const http = require("http");
 
 const app = experss();
-const server = http.createServer(app);
 const PORT = 3000 || process.env.PORT;
 
-app.use(experss.static("./public/html"));
-
-server.listen(PORT, () => {
-  console.log(`Server running in port ${PORT}`);
+const server = app.listen(PORT, (err) => {
+  if (err) {
+    console.log(`Whops an error pooped up... ${err}`);
+  } else {
+    console.log(`Server running in port ${PORT}`);
+  }
 });
+
+app.use(experss.static("./public"));
